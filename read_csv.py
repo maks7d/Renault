0ebc07fc239c1d8098a0ff41037055513efcd975
Maks <64848044+maks7d@users.noreply.github.com>
2023-04-13 11:26:53 +0200

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -1,12 +1,33 @@
 import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-with open(input('nom du fichier : ') + ".csv", 'r', newline='') as csvfile:
+nom_fichier = input('nom du fichier : ') + ".csv"
 
-    reader = csv.DictReader(csvfile)
+df = pd.read_csv(nom_fichier)
 
-    for row in reader:
-        puissance_tot = row['puissance tot']
-        gpu_freq = row['gpu freq']
+puissance_tot = df['puissance tot']
+ram_used = df['ram used']
+gpu_temp = df['gpu temp']
+cpu_temp = df['cpu temp']
+board_temp = df['board temp']
 
-        print(f" puissance totale : {puissance_tot} ")
-        print(f"gpu freq : {gpu_freq}")
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5,1)
+
+ax1.plot(puissance_tot)
+ax1.set_title("puissance tot (mW)")
+
+ax2.plot(ram_used)
+ax2.set_title("ram used (%)")
+
+ax3.plot(gpu_temp)
+ax3.set_title("gpu_temp (°C)")
+
+ax4.plot(cpu_temp)
+ax4.set_title("cpu temp (°C)")
+
+ax5.plot(board_temp)
+ax5.set_title("board temp (°C)")
+
+plt.show()
