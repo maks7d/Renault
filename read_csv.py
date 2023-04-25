536d0af3bb9406b3ce6743d0cd2d0bce4b7c3b7f
maks7d <maxime.deville@free.fr>
2023-04-25 17:50:39 +0200

    update 25/04

--- read_csv.py
+++ read_csv.py
@@ -3,16 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+#filtrage d'une liste en ne gardant que les valeurs comprises entre la moyenne et - et +*1.5l'ecart type
 def filter(s):
     mean = s.mean()
     std = s.std()
     return s[(s > mean - std) & (s < mean + 1.5*std)]
 
+#calcul du score de frugalité
 def frug(w, perf, puissance_moy):
-    return  (perf - round(perf,1)) - w/puissance_moy
+    return perf - w/(1+1/puissance_moy) 
+
 
 files_list = []
 
+#entrer nom de fichier tant que l'utilisateur rentre une chaine de caracteres
 while True:
     file_name = input("nom du fichier('Entrée pour terminer') : ")
     if not file_name:
@@ -23,6 +27,7 @@ frug_list = []
 
 fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5,1)
 
+#traitement du fichier et affichage des plot
 for file_name in files_list:
     df = pd.read_csv(file_name)
     puissance_tot = filter(df['puissance tot'])
@@ -33,13 +38,14 @@ for file_name in files_list:
     board_temp = filter(df['board temp'])
     perf = df['perf'][0]
 
-    for i in range(0,10):
+    frug_list.clear()
+    for i in range(0,1,10):
         frug_list.append(frug(i, perf, puissance_moy))
 
     ax1.plot(puissance_tot)
-    ax2.plot(puissance_moy)
-    ax3.plo(ram_used)
-    ax4.plot(gpu_temp)
-    ax5.plot(cpu_temp)
-
+    ax2.plot(ram_used)
+    ax3.plot(gpu_temp)
+    ax4.plot(cpu_temp)
+    ax5.plot(frug_list)
+    print(frug_list)
 plt.show()
